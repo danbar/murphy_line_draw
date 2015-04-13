99d6632d8253faa608dc4080032a5c848e9842d1
Daniel Bartel <danbar@users.noreply.github.com>
2015-04-13 21:06:36 +0200

    minor refactoring of demo

--- demo.m
+++ demo.m
@@ -1,42 +1,47 @@
-clc
+% A MATLAB script file containing a small demo for Murphy's line algorithm.
+%
+% Copyright (C) 2015 Daniel Bartel
+% Distributed under the GNU GPL v2. For full terms see the file LICENSE.
+%
 
+% Use a 100x100 matrix for line drawing,
 bitmap = zeros(100, 100);
 offset = 50;
 
-% part1
-pt0    = struct('x', offset+4, 'y', offset+2);
-pt1    = struct('x', offset+48, 'y', offset+20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+% ... draw a line in each octant,
+pt0    = struct('x', offset + 10, 'y', offset +  5);
+pt1    = struct('x', offset + 45, 'y', offset + 20);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 1st octant
 
-pt0    = struct('x', offset-4, 'y', offset-2);
-pt1    = struct('x', offset-48, 'y', offset-20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset +  5, 'y', offset + 10);
+pt1    = struct('x', offset + 20, 'y', offset + 45);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 2nd octant
 
-pt0    = struct('x', offset+4, 'y', offset-2);
-pt1    = struct('x', offset+48, 'y', offset-20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset -  5, 'y', offset + 10);
+pt1    = struct('x', offset - 20, 'y', offset + 45);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 3rd octant
 
-pt0    = struct('x', offset-4, 'y', offset+2);
-pt1    = struct('x', offset-48, 'y', offset+20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset - 10, 'y', offset +  5);
+pt1    = struct('x', offset - 45, 'y', offset + 20);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 4th octant
 
-% part2
-pt0    = struct('x', offset+2, 'y', offset+4);
-pt1    = struct('x', offset+20, 'y', offset+48);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset - 10, 'y', offset -  5);
+pt1    = struct('x', offset - 45, 'y', offset - 20);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 5th octant
 
-pt0    = struct('x', offset-2, 'y', offset-4);
-pt1    = struct('x', offset-20, 'y', offset-48);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset -  5, 'y', offset - 10);
+pt1    = struct('x', offset - 20, 'y', offset - 45);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 6th octant
 
-pt0    = struct('x', offset+2, 'y', offset-4);
-pt1    = struct('x', offset+20, 'y', offset-48);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset +  5, 'y', offset - 10);
+pt1    = struct('x', offset + 20, 'y', offset - 45);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 7th octant
 
-pt0    = struct('x', offset-2, 'y', offset+4);
-pt1    = struct('x', offset-20, 'y', offset+48);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 5);
+pt0    = struct('x', offset + 10, 'y', offset -  5);
+pt1    = struct('x', offset + 45, 'y', offset - 20);
+bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 8th octant
 
-
-imagesc(rot90(bitmap))
-axis equal
+% ... and plot the resulting matrix.
+imagesc(rot90(bitmap));
+axis([0 100 0 100]);
+title('Murphy''s line algorithm');
