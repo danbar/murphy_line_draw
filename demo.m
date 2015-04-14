37b4e3b7fe5bcde83fc867adfcff573111947f1f
Daniel Bartel <danbar@users.noreply.github.com>
2015-04-14 20:46:13 +0200

    refactor demo

--- demo.m
+++ demo.m
@@ -6,42 +6,24 @@
 
 % Use a 100x100 matrix for line drawing,
 bitmap = zeros(100, 100);
-offset = 50;
 
-% ... draw a line in each octant,
-pt0    = struct('x', offset + 10, 'y', offset +  5);
-pt1    = struct('x', offset + 45, 'y', offset + 20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 1st octant
+% ... draw lines in each octant,
+angle  = 15*pi/180;
+center = 50;
 
-pt0    = struct('x', offset +  5, 'y', offset + 10);
-pt1    = struct('x', offset + 20, 'y', offset + 45);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 2nd octant
-
-pt0    = struct('x', offset -  5, 'y', offset + 10);
-pt1    = struct('x', offset - 20, 'y', offset + 45);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 3rd octant
-
-pt0    = struct('x', offset - 10, 'y', offset +  5);
-pt1    = struct('x', offset - 45, 'y', offset + 20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 4th octant
-
-pt0    = struct('x', offset - 10, 'y', offset -  5);
-pt1    = struct('x', offset - 45, 'y', offset - 20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 5th octant
-
-pt0    = struct('x', offset -  5, 'y', offset - 10);
-pt1    = struct('x', offset - 20, 'y', offset - 45);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 6th octant
-
-pt0    = struct('x', offset +  5, 'y', offset - 10);
-pt1    = struct('x', offset + 20, 'y', offset - 45);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 7th octant
-
-pt0    = struct('x', offset + 10, 'y', offset -  5);
-pt1    = struct('x', offset + 45, 'y', offset - 20);
-bitmap = murphy_line_draw(bitmap, pt0, pt1, 3);                % 8th octant
+for n = 1:24
+    x = cos(n*angle); y = sin(n*angle);
+    
+    startpoint = struct('x', center + round(15*x), ...
+                        'y', center + round(15*y));
+    endpoint   = struct('x', center + round(45*x), ...
+                        'y', center + round(45*y));                    
+    thickness  = mod(n, 2) + 1;
+                
+    bitmap = murphy_line_draw(bitmap, startpoint, endpoint, thickness);
+end
 
 % ... and plot the resulting matrix.
 imagesc(rot90(bitmap));
-axis([0 100 0 100]);
+axis equal
 title('Murphy''s line algorithm');
